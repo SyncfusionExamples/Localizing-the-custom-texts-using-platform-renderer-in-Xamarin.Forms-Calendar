1166480fbc5dd852e5bf7a56c7e5402220c33a67
syncfusion <muniappan.subramanian@syncfusion.com>
2019-01-03 12:28:15 +0530

    Localization sample has been modified as per MVVM pattern.

--- LocalizingCustomTextPlatformRenderer/LocalizingCustomTextPlatformRenderer/CalendarBehavior.cs
+++ LocalizingCustomTextPlatformRenderer/LocalizingCustomTextPlatformRenderer/CalendarBehavior.cs
@@ -14,23 +14,10 @@ namespace LocalizingCustomTextPlatformRenderer
         protected override void OnAttachedTo(SfCalendar bindable)
         {
             calendar = bindable;
-            // Creating Events in SfCalendar
-            CalendarEventCollection calendarInlineEvents = new CalendarEventCollection();
-            calendar.ShowInlineEvents = true;
+           
             calendar.MoveToDate = new DateTime(2018, 02, 14, 12, 0, 0);
             // Set Locale to Calendar
             calendar.Locale = new System.Globalization.CultureInfo("fr-FR");
-
-            calendarInlineEvents.Add(new CalendarInlineEvent()
-            {
-                StartTime = new DateTime(2018, 02, 14, 12, 0, 0),
-                EndTime = new DateTime(2018, 02, 14, 02, 0, 0),
-                Subject = "Meeting",
-                IsAllDay = true
-            });
-
-            //Add collection of events as source of SfCalendar
-            calendar.DataSource = calendarInlineEvents;
 
             base.OnAttachedTo(bindable);
         }
